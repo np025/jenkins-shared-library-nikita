9249cd16bca0d348ac923567e17818645c09e905
np025 <130171068+np025@users.noreply.github.com>
2024-10-18 23:06:49 +0530

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -2,5 +2,5 @@ def call(String ImageName, String ImageTag, String dockerhubuser){
   withCredentials([usernamePassword(credentialsId: 'docker_hub_cred', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
       sh "docker login -u ${env.USER} -p ${env.PASS}"
   }
-  sh "docker push ${env.USER}/${ImageName}:${ImageTag}"
+  sh "docker push ${dockerhubuser}/${ImageName}:${ImageTag}"
 }
